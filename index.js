ba645e0a1144854b46d894aaf41594bef40b4b1e
Duman <dubikan@mail.ru>
2016-10-06 12:17:47 +0600

    First commit

---
+++ index.js
@@ -0,0 +1,45 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('soc-faf-bmList', []);
+
+  angular
+    .module('soc-faf-bmList')
+    .directive('bmList', bmList);
+
+  function bmList(AppModel) {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'list.directive.html',
+      scope: {},
+      controller: bmListCtrl,
+      controllerAs: 'bm',
+      bindToController: true
+    };
+    return directive;
+  };
+
+  bmListCtrl.$inject = ['AppModel', '$routeParams', '$location'];
+
+  function bmListCtrl(AppModel, $routeParams, $location) {
+    var bm = this;
+    bm.remove = remove;
+    bm.edit = edit;
+    bm.currentPage = 1;
+    bm.perPageCount = 5;
+
+    bm.vm = AppModel.get('all');
+
+    bm.filter = $routeParams.tag;
+
+    function remove(id){
+      AppModel.remove('list', id);
+    };
+
+    function edit(id){
+      $location.path('/edit/' + id);
+    };
+  };
+
+})();
